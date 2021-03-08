0a4a69450fe4ebe8298b65d9419b1f86b3582794
T-zemmari <76817211+T-zemmari@users.noreply.github.com>
2021-03-08 19:00:06 +0100

    added routes paths to home-login-register-profile-profile/appointment-profile/historial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Switch ,Route} from 'react-router-dom';
+ import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+         
+      <Switch>
+
+           <Route path='/' exact component={Home}/>
+           <Route path='/login' exact component ={Login}/>
+           <Route path='/register' exact component ={Register}/>
+           <Route path='/profile' exact component ={Profile}/>
+           <Route path='/profile/appointment' exact component ={Profile}/>
+           <Route path='/profile/historial' exact component ={Profile}/>
+
+      </Switch>
+         
+      </BrowserRouter>
+      
     </div>
   );
 }
