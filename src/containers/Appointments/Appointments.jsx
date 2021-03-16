e64c6db208dde7dfb0262fa9cba8812cdbb3b040
Fede <fedebaez@gmail.com>
2021-03-16 19:14:30 +0100

    Finished desktop design of my-appointments and added cancellation functionality.

--- src/containers/Appointments/Appointments.jsx
+++ src/containers/Appointments/Appointments.jsx
@@ -3,9 +3,9 @@ import Header from '../../components/Header/Header';
 import LeftMenu from '../../components/LeftMenu/LeftMenu';
 import {useHistory} from 'react-router-dom';
 import axios from 'axios';
+import moment from 'moment';
 import Message from '../../components/Message/Message';
 import Loading from '../../components/Loading/Loading';
-import './Appointments.scss';
 
 
 
@@ -49,25 +49,65 @@ const Appointments = (props) => {
         setMessage([...message,<Message key={key} text={msg} style={style}></Message>]);
     }
 
+    const mapDentist = (id) => {
+        switch (id) {
+            case 1:
+                return 'Dr. Iglesias Fornes David';
+            case 2:
+                return 'Dra. Flores Marín Lorena';
+            case 3:
+                return 'Dr. Zemmari Kissani Tarik';
+            case 4:
+                return 'Dra. Sanz Iglesias Noemi';
+        }
+    }
+
+    const cancelAppointment = (id) => {
+        let confirmed = confirm('¿Seguro que desea cancelar la cita?');
+        if (confirmed) {
+            setLoading(true);
+            setTimeout(()=>{
+                axios.delete(`http://localhost:3001/users/${credentials.user.id}/appointments/${id}`,{headers:{'authorization':'Bearer ' + credentials.token}})
+                .then(()=>axios.get(`http://localhost:3001/users/${credentials.user.id}/appointments`,{headers:{'authorization':'Bearer ' + credentials.token}}))
+                .then(handleResponse)
+                .then(()=>{newMessage('Cita cancelada con éxito','success')})
+                .catch((err)=>{handleResponse({data:{message:'Error de conexión.'}})});
+            },500);
+        }
+    }
+
     return (
         <>
         <Header></Header>
         <section className="appointmentsContainer">
-            {message}
             <LeftMenu></LeftMenu>
             <div className="appointments">
+                {message}
                 <Loading visible={loading}></Loading>
                 <div className="titulo">Mis citas</div>
                 {appointments.map((appointment)=>{
                     const key = (~(Math.random()*999999999));
+                    let dentist = mapDentist(appointment.dentistId) || 'Sin asignar';
+                    let cancel;
+                    if (moment(appointment.date).diff(new Date) > 0)
+                    cancel = <div id={appointment.id} onClick={(e)=>{cancelAppointment(e.target.id)}} className="cancel">Cancelar cita</div>;
                     return(
                         <div key={key} className="appointment">
                             <div className="date">
-                                {appointment.date}
+                                <div>{moment(appointment.date).format('D/M/Y')}</div>
+                                <div>{moment(appointment.date).format('H:mm')}</div>
                             </div>
-                            <div className="comment">
-                                {appointment.comment}
+                            <div className="data">
+                                <div className="row">
+                                    <div className="title">Dentista:</div>
+                                    <div className="datum">{dentist}</div>
+                                </div>
+                                <div className="row">
+                                    <div className="title">Comentarios:</div>
+                                    <div className="datum">{appointment.comment}</div>
+                                </div>
                             </div>
+                            {cancel}
                         </div>
                     );
                 })}
